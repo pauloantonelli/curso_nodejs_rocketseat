9b07f4ce966f30cfe3cd6897a1303a8b55874e3e
pauloantonelli <pauloantonelli@hotmail.com>
2019-02-07 15:59:07 -0200

    fim da aula :)

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 //iniciando a aplicacao
 const app = express();
@@ -7,9 +8,20 @@ const app = express();
 //iniciando o banco de dados
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
 
+//registrando modelo na aplicacao
+requireDir('./src/models');
+
+//criando primeiro produto no banco
+const product = mongoose.model('Product');
+
 //primeira rota criada
 app.get('/', (req, res) => {
-    res.send('Olá Mundo');
+    product.create({
+        title: 'Nativescript',
+        description: 'Build native apps with Angular 2+',
+        url: 'https://www.nativescript.org/',
+    })
+    res.send('Produto Requistrado com sucesso!');
 });
 
 //porta externa da aplicacao
