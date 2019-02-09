16d0b7163a80606dd847882968fad298c15a4c94
pauloantonelli <pauloantonelli@hotmail.com>
2019-02-09 11:22:04 -0200

    fim da aula

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -9,9 +9,28 @@ module.exports = {
 
         return res.json(products); // retorna os dados da base de dados em formato json
     },
+    async show(req, res) {
+        //mostra os dados de um item unico pela id
+        const products = await product.findById(req.params.id);
+
+        return res.json(products);
+    },
     async store(req, res) {
         // criação de novo registro
         const products = await product.create(req.body);//.body contem todas as informações dos dados da requisição
+
         return res.json(products);
+    },
+    async update(req, res) {
+        //acha o item pelo id e atualiza ele
+        const products = await product.findByIdAndUpdate(req.params.id, req.body, { new: true }); //o objeto new faz com que o dado jogado na variavel products seja o atualizado do banco, e nao o anterior
+
+        return res.json(products);
+    },
+    async delete(req, res) {
+        //acha o item pelo id e remove ele
+        await product.findByIdAndRemove(req.params.id);//como vamos deletar nao tem a necessidade de recuperar ele, a nao ser que queiramos exibir ou coisa parecida
+
+        return res.send();//retorna uma mensagem de sucesso vazia
     }
 };
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,9 @@ const routes = express.Router();
 const productController = require('./controllers/ProductController');
 
 routes.get('/products', productController.index);
+routes.get('/products/:id', productController.show);
 routes.post('/products', productController.store);
+routes.put('/products/:id', productController.update);
+routes.delete('/products/:id', productController.delete);
 
 module.exports = routes;
