7aec37594d8f5f28113cda963d6d637a44b9d7e9
pauloantonelli <pauloantonelli@hotmail.com>
2019-03-09 15:09:23 -0300

    fim da aula

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors');
 
 //iniciando a aplicacao
 const app = express();
+
+//adiciona regra de cors para acessos de outros softwares e de outros dominios
+app.use(cors());
 
 //instrui a aplicação trabalhar com requisições no formato json
 app.use(express.json());
